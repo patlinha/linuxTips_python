271e28227fbfe82a6441264d71219e11640e620c
patlinha <patylinha@yahoo.com.br>
2025-07-02 05:34:25 -0300

    múltiplos erros no except

--- errors.py
+++ errors.py
@@ -3,19 +3,25 @@
 import os
 import sys
 
-#LBYL - Look Before You Leap
+#EAFP - Easy to Ask Forgiveness than permission
 
 
-if os.path.exists('names.txt'):
-    print("O arquivo existe")
-    input("...") # Race Condition
-    names = open("names.txt").readlines()
-else:
-    print("[Error] File names.txt not found")
+try:
+    1 / 0 #ZeroDivisionError
+    names = open("names.txt").readlines() #FileNotFoundError
+    print(names.append) #AttributeError
+except (FileNotFoundError, ZeroDivisionError) as e:
+    print(f"{str(e)}")
+    sys.exit(1)
+except ZeroDivisionError:
+    print("[Error] You can't divide any number by 0")
+    sys.exit(1)
+except AttributeError:
+    print("[Error] This is a list not an object")
     sys.exit(1)
 
-if len(names) >= 4:
+try:
     print(names[3])
-else:
+except:
     print("Missing name in this position")
     sys.exit(1)
